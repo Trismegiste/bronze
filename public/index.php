ac8180eead5912ce32d9cd1d99cb222599fdfa3d
Trismegiste <flo@trismegiste.net>
2023-05-24 11:18:50 +0200

    testing tree component

--- public/index.php
+++ public/index.php
@@ -2,20 +2,12 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\FormBuilderInterface;
 use Trismegiste\Bronze\Core\BusinessApp;
 
 $app = new BusinessApp();
 
-$app->crud('bronze', 'bicycle', function (FormBuilderInterface $builder) {
-    return $builder
-            ->add('brand', TextType::class)
-            ->add('size', TextType::class)
-            ->add('color', TextType::class)
-            ->add('save', SubmitType::class)
-            ->getForm();
+$app->get('/', function () {
+    return $this->render('tree.html.twig', ['tree' => []]);
 });
 
 $app->run();
